afe8a46972fb91c79dc70e8d50decfee81d85d9f
Pallabi Chakraborty <pallavichakraborty@yahoo.com>
2019-03-28 11:00:33 -0500

    first commit

---
+++ server/server.js
@@ -0,0 +1,42 @@
+const express = require("express");
+const bodyParser=require("body-parser");
+const cookieParser=require("cookie-parser");
+const mongoose=require("mongoose");
+
+const app = express();
+require('dotenv').config();
+
+mongoose.Promise=global.Promise;
+mongoose.connect(process.env.DATABASE,{ 
+                                      useNewUrlParser: true,
+                                      useCreateIndex:true });
+
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.json());
+app.use(cookieParser());
+
+//Models
+const {User}=require('./models/user');
+
+//==========================================
+//           USERS
+//==========================================
+
+app.post('/api/users/register',(req,res)=> {
+    const user=new User(req.body);
+
+    user.save((err,doc) => {
+        if(err) return res.json({success:false,
+                                  err})
+        res.status(200).json({
+            success:true,
+            userdata:doc
+        })
+    })
+})
+
+const port=process.env.PORT||3002;
+
+app.listen(port,()=> {
+    console.log(`Server running at ${port}`)
+})
